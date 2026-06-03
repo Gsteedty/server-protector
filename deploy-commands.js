d1363c41c6e792903c107d17a77a6ac0da6cf2ee
Gsteedty <skibbidytoilet@icloud.com>
2026-06-02 21:18:20 -0400

    Add deploy-commands.js

---
+++ deploy-commands.js
@@ -0,0 +1,20 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord.js');
+const fs = require('fs');
+const path = require('path');
+
+const commands = [];
+const commandFiles = fs.readdirSync('./commands').filter((file) => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+  const filePath = path.join(__dirname, `./commands/${file}`);
+  const command = require(filePath);
+  commands.push(command.data.toJSON());
+}
+
+const rest = new REST({ version: '10' }).setToken(process.env.TOKEN);
+
+rest.put(Routes.applicationGuildCommands(process.env.CLIENT_ID, process.env.GUILD_ID), {
+  body: commands,
+}).then(() => console.log('Successfully registered application commands.'))
+.catch(console.error);
